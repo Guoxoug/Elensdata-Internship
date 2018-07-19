6bc73d5c958890aaa0c3b8b679bd5562b081e615
Guoxuan Xia <guoxuan_xia_1@hotmail.com>
2018-07-19 17:44:33 +0800

    Nothing works anymore in parallelisation going to try apply_async

--- People/excecute.py
+++ People/excecute.py
@@ -1,7 +1,7 @@
 from People import Generate
 import pandas as pd
 from timeit import default_timer
-overall_population_size = 100000
+overall_population_size = 10000000
 for i in range(25):
     start = default_timer()
     start_id = int(overall_population_size//25*i + 1)
@@ -14,10 +14,10 @@ for i in range(25):
     array = Generate.build_relationship_array(Pop)
     if i == 0:
         df = pd.DataFrame(array, columns = ['person1_id', 'rel_id', 'person2_id'])
-        df.to_csv('less_Relationships1.csv', index=False)
+        df.to_csv('less_Relationships.csv', index=False)
     else:
         df = pd.DataFrame(array)
-        df.to_csv('less_Relationships1.csv', mode="a", index=False, header=False)
+        df.to_csv('less_Relationships.csv', mode="a", index=False, header=False)
     #print(df)
 
     print("Time taken: ", default_timer() - start)
--- People/parallel.py
+++ People/parallel.py
@@ -2,40 +2,57 @@ from People import Generate
 from random import randint
 import numpy as np
 import multiprocessing as mp
-
+import pandas as pd
 from timeit import default_timer
+from People.person_class import person
 
 if __name__ == "__main__":
     print('tits')
     def generate_relationships_population_parallel(pop, mean_number: int, rang: int, num_groups: int):
         # cuts population into chunks and generates relationships within each chunk
         groups = np.array_split(pop, num_groups)
+        new_pop = []
 
         def generate_relationships_individual_parallel(individual):
-            # This needs to inside in order to use the global variables
-            individual.relationships = []
+            # This needs to inside in order to use the global variables which is a dodgy trick to remember
+            new_individual = person(individual.id)
+            new_individual.relationships = []
             population_size = len(group)
             i = 0
             n = randint(mean_number - rang, mean_number + rang)
+            new_individual.relationships.append("1")
             if n <= population_size:
                 while i < n:
                     index = randint(0, population_size - 1)
                     another_individual = group[index]
                     # picks a person object from the group
-                    if another_individual.id not in individual.relationships:
-                        individual.relationships.append(another_individual.id)
+                    if another_individual.id not in new_individual.relationships:
+                        new_individual.relationships.append(another_individual.id)
                         i += 1
             else:
                 pass
-
+            return new_individual
+        def generate_relationships_inside(pop, mean_number: int, rang: int, num_groups: int):
+            for individual in group:
+                Generate.generate_relationships_individual(individual, )
         for group in groups:
-            mp.Pool().imap_unordered(generate_relationships_individual_parallel, group, len(group) // mp.cpu_count())
+            group.tolist()
+            for individual in group:
+                new_pop.append(mp.Pool().apply_async())
 
     # Calling previously defined functions and timing it
     start = default_timer()
-    overall_population_size = 10000000
+    overall_population_size = 1000000
 
     Pop = Generate.generate_population(overall_population_size)
 
-    generate_relationships_population_parallel(Pop, 3, 2, 10)
+    Pop = generate_relationships_population_parallel(Pop, 3, 2, 10)
+
+
+    array = Generate.build_relationship_array(Pop)
+
+    df = pd.DataFrame(array, columns=['person1_id', 'rel_id', 'person2_id'])
+    df.to_csv('less_Relationships1.csv', index=False)
+
     print("time taken: ", default_timer() - start)
+    print(Pop[0])
