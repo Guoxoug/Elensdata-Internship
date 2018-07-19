a1d13721e4efbc3e87db36f16e7e49df2c5a3092
Guoxuan Xia <guoxuan_xia_1@hotmail.com>
2018-07-19 18:14:18 +0800

    encountered pickle error

--- People/parallel.py
+++ People/parallel.py
@@ -8,12 +8,14 @@ from People.person_class import person
 
 if __name__ == "__main__":
     print('tits')
+
+
     def generate_relationships_population_parallel(pop, mean_number: int, rang: int, num_groups: int):
         # cuts population into chunks and generates relationships within each chunk
         groups = np.array_split(pop, num_groups)
         new_pop = []
 
-        def generate_relationships_individual_parallel(individual):
+        def generate_relationships_individual_parallel(individual, mean_number, rang, group):
             # This needs to inside in order to use the global variables which is a dodgy trick to remember
             new_individual = person(individual.id)
             new_individual.relationships = []
@@ -31,14 +33,39 @@ if __name__ == "__main__":
                         i += 1
             else:
                 pass
+            # needs to return something otherwise multiprocessing won't work
             return new_individual
+
         def generate_relationships_inside(pop, mean_number: int, rang: int, num_groups: int):
-            for individual in group:
-                Generate.generate_relationships_individual(individual, )
-        for group in groups:
-            group.tolist()
-            for individual in group:
-                new_pop.append(mp.Pool().apply_async())
+            population_chunk = []
+            for individual in pop:
+                new_individual = person(individual.id)
+                new_individual.relationships = []
+                population_size = len(pop)
+                i = 0
+                n = randint(mean_number - rang, mean_number + rang)
+                new_individual.relationships.append("1")
+                if n <= population_size:
+                    while i < n:
+                        index = randint(0, population_size - 1)
+                        another_individual = pop[index]
+                        # picks a person object from the group
+                        if another_individual.id not in new_individual.relationships:
+                            new_individual.relationships.append(another_individual.id)
+                            i += 1
+                population_chunk.append(new_individual)
+            return population_chunk
+
+        with mp.Pool() as pool:
+            for group in groups:
+                group.tolist()
+
+                new_pop += pool.apply_async(generate_relationships_inside, (group, mean_number,rang,
+                                                                                num_groups,)).get()
+
+            pool.close()
+            pool.join()
+        return new_pop
 
     # Calling previously defined functions and timing it
     start = default_timer()
@@ -47,7 +74,6 @@ if __name__ == "__main__":
     Pop = Generate.generate_population(overall_population_size)
 
     Pop = generate_relationships_population_parallel(Pop, 3, 2, 10)
-
 
     array = Generate.build_relationship_array(Pop)
 
